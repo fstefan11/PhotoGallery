78f0924036ca1513714782b3fa7202919471087a
Stefan <fstefan915@gmail.com>
2025-02-24 15:57:51 +0200

    Verificare la conectare cu baza de date daca este facuta deja conexiunea si incercare de conectare la fiecare metoda care face cerere catre baza de date

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { getProfilePic } from "@/lib/actions/userActions";
-import { CldImage, CldUploadButton } from "next-cloudinary";
 import UploadImage from "@/components/cloudinaryUploadComponent";
 import Providers from "@/components/providersComponent";
 import ProfilePicture from "@/components/profilePictureComponent";
@@ -14,12 +13,6 @@ export default async function Dashboard() {
       <div className="text-3xl">Dashboard</div>
       <br />
       <br />
-      {/* <img
-        src={profilePic ? profilePic : "/noprofilepic.webp"}
-        alt="Profile picture"
-        width={300}
-        height={300}
-      /> */}
       {profilePic && <ProfilePicture publicId={profilePic} />}
       <Providers>
         <UploadImage>Change profile picture</UploadImage>
--- components/profilePictureComponent.js
+++ components/profilePictureComponent.js
@@ -4,14 +4,15 @@ import { CldImage } from "next-cloudinary";
 
 export default function ProfilePicture({ publicId }) {
   return (
-    <div className="relative w-96 h-96">
-      <CldImage
+    <div className="relative w-[600px]">
+      {/* <CldImage
         src={publicId}
         alt="Profile picture"
-        crop="fit"
+        crop="fill"
         fill
         sizes="100vw"
-      />
+      /> */}
+      <img src={publicId}></img>
     </div>
   );
 }
